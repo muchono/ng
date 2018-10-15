bdc2c795b057f9e28a8ce66904825d7478f1ee06
Valeriy Ermolin <vermolin@geeksforless.net>
2018-10-15 13:27:03 +0300

    update

--- protected/extensions/Bitcoin/Bitcoin.php
+++ protected/extensions/Bitcoin/Bitcoin.php
@@ -118,6 +118,19 @@ class Bitcoin extends CPayment
         return $address;
     }
     
+    public function getAddressGap()
+    {
+        $url = 'https://api.blockchain.info/v2/receive/checkgap?xpub='.$this->_params['xpub'].'&key='.$this->_params['key'];
+        
+        $curl = new Curl();
+        $agent= 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1; .NET CLR 1.0.3705; .NET CLR 1.1.4322)';
+        $res = $curl->setOption(CURLOPT_SSL_VERIFYPEER, false)
+                    ->setOption(CURLOPT_FOLLOWLOCATION, false)
+                    ->setOption(CURLOPT_USERAGENT, $agent)
+                    ->get($url);
+        return json_decode($res, true);
+    }
+    
     /**
      * Get payments
      * @param integer $user_id User ID
